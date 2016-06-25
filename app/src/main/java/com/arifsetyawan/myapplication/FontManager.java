b8d7d91a293caf1eaf20924804a91c9cb24cf7bb
arif setyawan <me@arifsetyawan.com>
2016-06-25 10:23:49 +0800

    Interactivity

---
+++ app/src/main/java/com/arifsetyawan/myapplication/FontManager.java
@@ -0,0 +1,35 @@
+package com.arifsetyawan.myapplication;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by arifsetyawan on 6/12/16.
+ */
+public class FontManager {
+
+    public static final String ROOT = "fonts/",
+            FONTAWESOME = ROOT + "fontawesome-webfont.ttf";
+
+    public static Typeface getTypeface(Context context, String font) {
+        return Typeface.createFromAsset(context.getAssets(), font);
+    }
+
+    public static void markAsIconContainer(View v, Typeface typeface) {
+        if (v instanceof ViewGroup) {
+            ViewGroup vg = (ViewGroup) v;
+            for (int i = 0; i < vg.getChildCount(); i++) {
+                View child = vg.getChildAt(i);
+                //markAsIconContainer(child);
+            }
+        } else if (v instanceof TextView) {
+            ((TextView) v).setTypeface(typeface);
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/arifsetyawan/myapplication/MainActivity.java
+++ app/src/main/java/com/arifsetyawan/myapplication/MainActivity.java
@@ -1,13 +1,64 @@
 package com.arifsetyawan.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    protected double itemPrice = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
+        //FontManager.markAsIconContainer(findViewById(R.id.icons_container), iconFont);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Menambah jumlah barang
+     */
+    public void increaseItem(View view) {
+        display(1);
+    }
+
+    /**
+     * Mengurangi jumlah barang
+     * @param view
+     */
+    public void decreaseItem(View view){
+        display(-1);
+    }
+
+    public void orderItem(){
+
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view); // initial value 0
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view); // initial value 0.00
+
+        int existing = Integer.parseInt(String.valueOf(quantityTextView.getText()));
+        number = number+existing;
+        if(number < 1) number = 0; // mencegah value negatif
+
+        double price = number * itemPrice;
+
+        quantityTextView.setText("" + number);
+        priceTextView.setText("" + NumberFormat.getCurrencyInstance().format(price));
+
+    }
+
 }
